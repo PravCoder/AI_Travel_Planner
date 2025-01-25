f89e6244083a456109b6e441284f69ff3e139ca5
Pravachan Patra <pravachanpatra@gmail.com>
2025-01-25 08:37:05 -0600

    create a test api endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,12 @@ const cors = require("cors")
 const axios = require("axios");
 
 const app = express()
+const router = express.Router();
 app.use(express.json())
 app.use(cors())
+app.use("/", router);
+
+
 
 // connection string with db-password db-name, password is leaked here, also manually have to put db-name here after .net/
 mongoose.connect("mongodb+srv://admin:djbeg123*@aitravelplannercluster.96bpw.mongodb.net/?retryWrites=true&w=majority&appName=aitravelplannercluster")
@@ -16,3 +20,18 @@ mongoose.connect("mongodb+srv://admin:djbeg123*@aitravelplannercluster.96bpw.mon
 app.listen(3001, () => {
     console.log("server is running")
 })
+
+
+// our api endpoint that is requested by frontend, handle logic here, sends response with data back to frontend for displaying
+router.post("/test", async (req, res) => {
+    try {
+        const {} = req.body; 
+        
+        // return some data in our response to the reqest. 
+        res.status(201).json({message: "test successfully created", test_data: "testing 123.."});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "unable to test, error: error.messag" });
+    }
+
+});
