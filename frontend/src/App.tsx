dfac276c6cefa3a1e2fdd87bcab38cd8d51fdee9
coltonsellers <coltonsellers20@outlook.com>
2025-02-28 20:09:40 -0600

    Remove default css margin and padding inconsistencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardLayoutBasic from "./components/DashboardLayout";
 import DashboardPage from "./pages/DashboardPage";
 import AboutPage from "./pages/AboutPage";
+import "./styles/global.css";
 
 export default function App() {
   return (
