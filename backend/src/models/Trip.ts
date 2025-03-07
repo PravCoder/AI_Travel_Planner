337217df6a4199f2b84580b8974858f7e8e11af7
BDarbZ <robert.bradford.darby@gmail.com>
2025-03-06 18:02:47 -0600

    Defined Trip.ts to include attributes and fields. May need to have things added.

--- backend/src/models/Trip.ts
+++ backend/src/models/Trip.ts
@@ -1,1 +1,48 @@
-/*  Define the Trip class model here. */
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+// define the interface for the Trip schema 
+interface ITrip extends Document {
+  title: string;
+  hours: Date; 
+  activityType: string;
+  cost: number;
+  rating: number;
+  cuisine: string;
+  location: {
+    type: string;
+    coordinates: [number, number];
+  };
+  user: Schema.Types.ObjectId; // Reference to the User who created the trip
+}
+
+// define the schema for the Trip
+const TripSchema: Schema<ITrip> = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    hours: { type: Date, required: true }, 
+    activityType: { type: String, required: true },
+    cost: { type: Number, required: true }, 
+    rating: { type: Number, required: true }, 
+    cuisine: { type: String, required: true }, 
+    location: {
+      type: {
+        type: String,
+        enum: ['Point'], 
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true } // Reference to the User model
+  },
+);
+
+// Add a geospatial index for location-based queries
+TripSchema.index({ location: '2dsphere' });
+
+// define the model using the interface and schema
+const TripModel: Model<ITrip> = mongoose.model<ITrip>("Trip", TripSchema);
+
+export default TripModel;
