5ade7b81abb2b584aa97425c0ee069e4874cc948
BDarbZ <robert.bradford.darby@gmail.com>
2025-04-22 12:49:14 -0500

    Added a toJSON transform so that when JSON is sent back, the password field is deleted.

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -16,9 +16,17 @@ const UserSchema: Schema<IUser> = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false }, //making sure password is not fetched by default.
     trips: [{type: Schema.Types.ObjectId, ref: "Trip" }],
   },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;   // removes password for all json formats.
+        return ret;
+      }
+    }
+  }
 );
 
 // define the model using the interface and schema
